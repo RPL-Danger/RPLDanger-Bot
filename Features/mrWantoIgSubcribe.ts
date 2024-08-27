dd443e3a9171bf3ca837d925e112eea31809a563
mraihanaf <mraihanaf0@gmail.com>
2024-08-27 13:31:42 +0700

    feat: add Features folder

---
+++ Features/mrWantoIgSubcribe.ts
@@ -0,0 +1,8 @@
+import { schedule } from "node-cron"
+import { Client } from "discord.js"
+export default {
+    /* Cek Instagram pak wanto setiap 5 menit */
+    load: async (client: Client) => {
+        
+    }
+}
