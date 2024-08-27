8758b1d631ded482f35ca2aab266cba44d5c64af
mraihanaf <mraihanaf0@gmail.com>
2024-08-27 13:30:00 +0700

    feat: setup database

---
+++ Models/index.ts
@@ -0,0 +1,2 @@
+/* Database models */
+// Export all models
--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import { config } from "dotenv"
 import loadHandlers from "./Handlers"
-import { Client, Events, GatewayIntentBits } from "discord.js"
+import { Client, GatewayIntentBits } from "discord.js"
+import { connect } from "mongoose";
 
-config()
+config();
 
-/* This like permission or smthng idk */
+(async () => {
+    /* This like permission or smthng idk */
+    const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
-loadHandlers(client)
-client.login(process.env.TOKEN)
+    // connect to the database
+    await connect(process.env.MONGODB_URI!)
+    console.log("Connected to the Database.")
+    
+    // load all handlers
+    await loadHandlers(client)
+
+    await client.login(process.env.TOKEN)
+
+})().catch(err => {
+    console.error(err)
+})
