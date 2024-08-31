40df050305333f648786957d2f7abc3e1d6dcdf3
mraihanaf <mraihanaf0@gmail.com>
2024-09-01 01:13:41 +0700

    refactor: use ig.createEmbed()

--- Commands/mrWantofetch.ts
+++ Commands/mrWantofetch.ts
@@ -1,22 +1,14 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits } from "discord.js";
 export default {
     data: new SlashCommandBuilder()
           .setName("mrwantofetch")
-          .setDescription("Cek post terbaru Mr. Wanto"),
+          .setDescription("Cek post terbaru Mr. Wanto")
+          .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
     execute: async (interaction: ChatInputCommandInteraction) => {
         const ig = interaction.client.instagram
         const {data: latestPost} = await ig.getLatestPost(ig.mrWantoId)
         const postUrl: string = `https://instagram.com/p/${latestPost.code}`
-        const mediaUrl = latestPost.image_versions2.candidates[0].url
-        const embed = new EmbedBuilder()
-        .setColor(0x0099FF)
-        .setDescription(latestPost.caption?.text ? latestPost.caption.text : "No Caption")
-        .setAuthor({name:"@wantoariwibowo"})
-        .addFields(
-            { name: 'Likes', value: latestPost.like_count.toString() },
-            { name: 'Comments', value: latestPost.comment_count.toString() },
-        )
-        .setImage(mediaUrl)
+        const embed = ig.createEmbed(latestPost)
         interaction.reply({content: postUrl, embeds: [embed]})
     }
 }
