833fd1e74cdbbb1a76cd1de392d8f5ae4f8eccf0
mraihanaf <mraihanaf0@gmail.com>
2024-09-01 01:15:20 +0700

    feat(mrWanto): add removeMrWantoChannel command

---
+++ Commands/removeMrWantoChannel.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, ChannelType, PermissionFlagsBits } from "discord.js"
+import channels from "../Models/MrWantoIGSubscribe"
+export default {
+    data: new SlashCommandBuilder()
+              .setName("setMrWantoChannel")
+              .setDescription("Set Mr Wanto Subscribe Channel.")
+              .addChannelOption(option => option
+                .setName("subscribe-channel")
+                .setDescription("text-channel")
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)    
+            )
+            .addStringOption(option => option
+                .setName("custom-message")
+                .setDescription("Custom Message")
+                .setRequired(false)
+            )
+            .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        const guild = await channels.findOne({guildId: interaction.guildId}).exec()
+        if(!guild) return await interaction.reply(`Sorry, Server Belom Ada Di Database`)
+        const channelId = guild.channelId
+        await channels.deleteOne({guildId: interaction.guildId})
+        await interaction.reply(`Berhasil Menghapus Channel #<${channelId}>`)
+    }   
+}
