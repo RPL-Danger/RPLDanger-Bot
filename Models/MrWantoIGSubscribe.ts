0dff510ed07df91f607307f986ed7e4cefac55e9
mraihanaf <mraihanaf0@gmail.com>
2024-09-01 01:11:01 +0700

    refactor(models): remove lastPost property

--- Models/MrWantoIGSubscribe.ts
+++ Models/MrWantoIGSubscribe.ts
@@ -4,11 +4,7 @@ import { IMrWantoIGSubscribe } from "../Types";
 const MrWantoIGSubscribe = new Schema<IMrWantoIGSubscribe>({
     guildId: {type: String, required: true},
     channelId: {type: String, required: true},
-    customMessage: {type: String},
-    lastPost: {
-        latestPostId: {type: String},
-        count: {type: Number}
-    }
+    customMessage: {type: String}
 })
 
 const mrWantoDb = model('MrWantoIGSubscribe', MrWantoIGSubscribe)
