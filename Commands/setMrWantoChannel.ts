7a5dc29d0e579f75a9bdb70f3de08bacfc695723
mraihanaf <mraihanaf0@gmail.com>
2024-09-01 01:15:45 +0700

    feat(mrWanto): add setMrWantoChannel command

---
+++ Commands/setMrWantoChannel.ts
@@ -0,0 +1,29 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, ChannelType, PermissionFlagsBits } from "discord.js"
+import channels from "../Models/MrWantoIGSubscribe"
+export default {
+    data: new SlashCommandBuilder()
+              .setName("setMrWantoChannel")
+              .setDescription("Set Mr Wanto Subscribe Channel.")
+              .addChannelOption(option => option
+                .setName("subscribe-channel")
+                .setDescription("text-channel")
+                .addChannelTypes(ChannelType.GuildText)
+                .setRequired(true)    
+            )
+            .addStringOption(option => option
+                .setName("custom-message")
+                .setDescription("Custom Message")
+                .setRequired(false)
+            )
+            .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        const guild = await channels.findOne({guildId: interaction.guildId}).exec()
+        if(guild) return await interaction.reply(`Sorry, Server Ini Sudah Ada Di Database. Silakan Hapus Terlebih Dahulu Menggunakan command /removeMrWantoChannel`)
+        await channels.create({
+            guildId: interaction.guildId,
+            channelId: interaction.options.getChannel('subscribe-channel')?.id,
+            customMessage: interaction.options.getString("custom-message") ? interaction.options.getString("custom-message") : "@everyone Ada Posts Baru Nih Dari Mr. Wanto!"
+        })
+        await interaction.reply(`Berhasil. Sekarang Posts Baru Mr. Wanto Akan Di Umumkan Di #<${interaction.options.getChannel("subscribe-channel")?.id}>`)
+    }   
+}
