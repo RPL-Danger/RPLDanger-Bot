2bdda3bc42d1b06cca13e288bddaf8d661a41e62
mraihanaf <mraihanaf0@gmail.com>
2024-09-02 01:48:09 +0700

    feat(ping): add latency information

--- Commands/ping.ts
+++ Commands/ping.ts
@@ -1,9 +1,13 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js"
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client } from "discord.js"
 export default {
     data: new SlashCommandBuilder()
               .setName("ping")
               .setDescription("Send a ping command!"),
     execute: async (interaction: ChatInputCommandInteraction) => {
-        await interaction.reply("Pong!")
+        const client: Client = interaction.client
+        await interaction.deferReply()
+        const reply = await interaction.fetchReply()
+        const ping: number = reply.createdTimestamp - interaction.createdTimestamp
+        await interaction.editReply(`Pong!\nClient \`${ping}ms\` | Websocket: \`${client.ws.ping}ms\``)
     }
 }
