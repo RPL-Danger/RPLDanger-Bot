d9d78f3a10cc448eae443eb0db4477c5e0cc37d4
mraihanaf <mraihanaf0@gmail.com>
2024-09-03 15:02:24 +0700

    feat(wouldyourather): add wouldyourather model

---
+++ Models/WouldYouRather.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+import { IOption, IWouldYouRather } from "../Types";
+
+const OptionSchema = new Schema<IOption>({
+    translate: { type: String },
+    original: { type: String },
+    votes: { type: Number }
+})
+
+const WouldYouRatherSchema = new Schema<IWouldYouRather>({
+    options: { type: [OptionSchema], required: true }
+})
+
+const WouldYouRatherModel = model('wouldyourathers', WouldYouRatherSchema)
+export default WouldYouRatherModel
