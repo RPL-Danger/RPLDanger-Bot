d03ca1625b24dacc24a5e605311b0a40ad79cd44
mraihanaf <mraihanaf0@gmail.com>
2024-09-06 04:55:38 +0700

    feat(imagery) add imagery types

---
+++ Types/imagery.d.ts
@@ -0,0 +1,6 @@
+export type ChannelId = string;
+export type GuildId = string;
+export interface IImageryMedia {
+    type: "IMAGE"|"YOUTUBE_URL",
+    url: string
+}
--- Types/index.ts
+++ Types/index.ts
@@ -2,3 +2,4 @@ export * from "./handlers"
 export * from "./MrWantoSubscribe"
 export * from "./latestPost"
 export * from './wouldyourather'
+export * from "./imagery"
